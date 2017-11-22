98c10e1fe90542bf92ead7e9664f651cf9016aa2
stramak <shtramak@gmail.com>
2017-11-22 17:33:34 +0200

    #19: add simpleSQL() method to Application, imlement connection to sqlcmd

--- src/main/java/juja/sqlcmd/Application.java
+++ src/main/java/juja/sqlcmd/Application.java
@@ -1,7 +1,38 @@
 package juja.sqlcmd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Application {
     public static void main(String[] args) {
+        new Application().simpleSQL();
+    }
 
+    void simpleSQL() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC driver not found!");
+            return;
+        }
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/sqlcmd", "sqlcmd", "sqlcmd");
+            System.out.println("You've get a connection to sqlcmd");
+        } catch (SQLException e) {
+            System.out.println("Something goes wrong. Connection failed!");
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
+    private void closeConnection(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
