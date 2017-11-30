d5574f141a5cb3b49085c61b0e70a53ae9f94a71
AVM82 <1982AlexVM@gmail.com>
2017-11-30 14:40:02 +0200

    #22:refactored

--- src/main/java/juja/sqlcmd/Application.java
+++ src/main/java/juja/sqlcmd/Application.java
@@ -1,6 +1,11 @@
 package juja.sqlcmd;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.DriverManager;
 
 public class Application {
 
