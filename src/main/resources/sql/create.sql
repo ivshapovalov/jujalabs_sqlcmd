a826a6fb31aa9d86bccf2dc2267177562a7995b1
stramak <shtramak@gmail.com>
2017-12-05 16:16:58 +0200

    #18: modify tables acc. to last version of ER model

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -1,40 +1,37 @@
-CREATE TABLE IF NOT EXISTS "shop" (
+CREATE TABLE "shop" (
   "id"      SERIAL PRIMARY KEY,
   "name"    VARCHAR(255)        NOT NULL,
   "address" VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "customer" (
+CREATE TABLE "customer" (
   "id"      SERIAL PRIMARY KEY,
   "name"    VARCHAR(255) NOT NULL,
   "surname" VARCHAR(255) NOT NULL,
-  "age"     SMALLINT     NOT NULL,
-  "shop_id" INTEGER      NOT NULL,
-  FOREIGN KEY ("shop_id") REFERENCES "shop" ("id")
+  "age"     SMALLINT     NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "purchase" (
+CREATE TABLE "purchase" (
   "id"          SERIAL PRIMARY KEY,
   "date"        TIMESTAMP NOT NULL,
   "customer_id" INTEGER   NOT NULL,
-  UNIQUE ("date", "customer_id"),
+  "shop_id"     INTEGER   NOT NULL,
+  FOREIGN KEY ("shop_id") REFERENCES "shop" ("id"),
   FOREIGN KEY ("customer_id") REFERENCES "customer" ("id")
 );
 
-CREATE TABLE IF NOT EXISTS "product" (
+CREATE TABLE "product" (
   "id"    SERIAL PRIMARY KEY,
-  "name"  VARCHAR(255) NOT NULL,
-  "price" MONEY        NOT NULL
+  "name"  VARCHAR(255)   NOT NULL,
+  "price" NUMERIC(19, 2) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "purchase_detail" (
-  "id"          SERIAL PRIMARY KEY,
-  "quantity"    INT2    NOT NULL,
-  "price"       MONEY   NOT NULL,
-  "shop_id"     INTEGER NOT NULL,
-  "purchase_id" INTEGER NOT NULL,
-  "product_id"  INTEGER NOT NULL,
-  FOREIGN KEY ("product_id") REFERENCES "product" ("id"),
-  FOREIGN KEY ("purchase_id") REFERENCES "purchase" ("id"),
-  FOREIGN KEY ("shop_id") REFERENCES "shop" ("id")
+CREATE TABLE "purchase_detail" (
+  "purchase_id" INTEGER        NOT NULL,
+  "product_id"  INTEGER        NOT NULL,
+  "quantity"    SMALLINT       NOT NULL,
+  "sell_price"  NUMERIC(19, 2) NOT NULL,
+  FOREIGN KEY ("purchase_id") REFERENCES purchase ("id"),
+  FOREIGN KEY ("product_id") REFERENCES product ("id"),
+  PRIMARY KEY ("purchase_id", "product_id")
 );
