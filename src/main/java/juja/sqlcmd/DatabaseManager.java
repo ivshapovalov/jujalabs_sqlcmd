ddf1f421c4646dc8dab926b4512470ca01974682
AVM82 <1982AlexVM@gmail.com>
2017-12-11 12:41:43 +0200

    #33: implemented getTableNames method

--- src/main/java/juja/sqlcmd/DatabaseManager.java
+++ src/main/java/juja/sqlcmd/DatabaseManager.java
@@ -3,10 +3,15 @@ package juja.sqlcmd;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class DatabaseManager {
     private static final String PORT = "5432";
     private static final String SERVER = "localhost";
+    private static final String TABLE_NAMES_QUERY = "SELECT relname FROM pg_stat_user_tables ORDER BY relname";
+    private static final String TABLE_ROWS_COUNT_QUERY = "SELECT count(*) FROM ?";
+    private static final int VALID_TIMEOUT = 15;
     private Connection connection;
 
     public boolean connect(String database, String user, String password) {
@@ -17,6 +22,45 @@ public class DatabaseManager {
             return true;
         } catch (ClassNotFoundException | SQLException e) {
             return false;
+        }
+    }
+
+    public String[] getTableNames() {
+        String[] tableNames = new String[0];
+        try {
+            if (connection != null && connection.isValid(VALID_TIMEOUT)) {
+                try (Statement statement = connection.createStatement();
+                     ResultSet resultSet = statement.executeQuery(TABLE_NAMES_QUERY)) {
+                    if (resultSet.isBeforeFirst()) {
+                        int arraySize = getTableRowsCount("pg_stat_user_tables");
+                        tableNames = new String[arraySize];
+                        for (int index = 0; resultSet.next(); index++) {
+                            tableNames[index] = resultSet.getString(1);
+                        }
+                    }
+                }
+            } else {
+                throw new SQLException("The connection to the database is not established");
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        return tableNames;
+    }
+
+    private int getTableRowsCount(String tableName) throws SQLException {
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(TABLE_ROWS_COUNT_QUERY.replace("?",tableName))) {
+            resultSet.next();
+            return resultSet.getInt(1);
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.getMessage();
         }
     }
 }
