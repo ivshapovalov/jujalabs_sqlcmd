bcbf6f597e64afb6ca28a099718360d0411f24a6
stramak <shtramak@gmail.com>
2017-12-14 17:40:28 +0200

    #44: implement tests for getTableData() method + minor changes in test environment preparation

--- src/test/java/juja/sqlcmd/DatabaseManagerTest.java
+++ src/test/java/juja/sqlcmd/DatabaseManagerTest.java
@@ -1,5 +1,6 @@
 package juja.sqlcmd;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -10,8 +11,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class DatabaseManagerTest {
@@ -37,13 +40,20 @@ public class DatabaseManagerTest {
     @AfterClass
     public static void closeConnection() throws SQLException {
         connection.close();
+        connection = DriverManager.getConnection(DB_CONNECTION_URL + DB_NAME, DB_USER, DB_USER_PASSWORD);
+        executeSqlQuery("DROP DATABASE IF EXISTS " + TEST_DB_NAME);
+        connection.close();
     }
 
     @Before
     public void init() throws SQLException {
         dropAllTables();
         databaseManager = new DatabaseManager();
-        databaseManager.connect(TEST_DB_NAME, DB_USER, DB_USER_PASSWORD);
+    }
+
+    @After
+    public void closeDbManagerConnection() throws SQLException {
+        databaseManager.closeConnection();
     }
 
     @Test
@@ -68,16 +78,53 @@ public class DatabaseManagerTest {
 
     @Test
     public void getTableNamesWhenNoTablesInDbReturnsEmptyArray() throws SQLException {
+        databaseManager.connect(TEST_DB_NAME, DB_USER, DB_USER_PASSWORD);
         String[] expected = new String[]{};
         assertArrayEquals(expected, databaseManager.getTableNames());
     }
 
     @Test
     public void getTableNamesWhenTwoTablesInDbReturnsTableNamesArray() throws SQLException {
+        databaseManager.connect(TEST_DB_NAME, DB_USER, DB_USER_PASSWORD);
         executeSqlQuery("CREATE TABLE table1()");
         executeSqlQuery("CREATE TABLE table2()");
         String[] expected = new String[]{"table1", "table2"};
         assertArrayEquals(expected, databaseManager.getTableNames());
+    }
+
+    @Test
+    public void getTableDataWhenEmptyTableReturnsEmptyArray() throws SQLException {
+        databaseManager.connect(TEST_DB_NAME, DB_USER, DB_USER_PASSWORD);
+        executeSqlQuery("CREATE TABLE test_table()");
+        DataSet[] expected = new DataSet[]{};
+        assertArrayEquals(expected, databaseManager.getTableData("test_table()"));
+    }
+
+    @Test
+    public void getTableDataWhenWrongTableNameReturnsEmptyArray() throws SQLException {
+        databaseManager.connect(TEST_DB_NAME, DB_USER, DB_USER_PASSWORD);
+        DataSet[] expected = new DataSet[]{};
+        assertArrayEquals(expected, databaseManager.getTableData("WrongTableName"));
+    }
+
+    @Test
+    public void getTableDataWhenValidDataReturnsTableDataArray() throws SQLException {
+        databaseManager.connect(TEST_DB_NAME, DB_USER, DB_USER_PASSWORD);
+        executeSqlQuery("CREATE TABLE test_table(" +
+                "id INTEGER," +
+                "name VARCHAR(128)" +
+                ")");
+        executeSqlQuery("INSERT INTO test_table VALUES(1,'name1')");
+        executeSqlQuery("INSERT INTO test_table VALUES(2,'name2')");
+        DataSet row1 = new DataSet(2);
+        row1.insertValue(0, "1");
+        row1.insertValue(1, "name1");
+        DataSet row2 = new DataSet(2);
+        row2.insertValue(0, "2");
+        row2.insertValue(1, "name2");
+        DataSet[] expected = new DataSet[]{row1, row2};
+        DataSet[] actual = databaseManager.getTableData("test_table");
+        assertThat(actual, arrayContainingInAnyOrder(expected));
     }
 
     private static void executeSqlQuery(String sqlQuery) throws SQLException {
