a71cee0d0bcd98f6141455e4341612f530ce8634
DenysMelnychenko <aspirant89@gmail.com>
2017-12-15 14:52:42 +0200

    #34 Created getTableNames method and appropriate tests

--- src/main/java/juja/sqlcmd/DatabaseManager.java
+++ src/main/java/juja/sqlcmd/DatabaseManager.java
@@ -2,7 +2,10 @@ package juja.sqlcmd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseManager {
     private static final String DB_CONNECTION_URL = "jdbc:postgresql://127.0.0.1:5432/";
@@ -23,5 +26,30 @@ public class DatabaseManager {
             return false;
         }
         return true;
+    }
+
+    public String[] getTableNames() throws SQLException {
+        String sqlQuery = "SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE'";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery, ResultSet.TYPE_SCROLL_SENSITIVE,
+                ResultSet.CONCUR_UPDATABLE); ResultSet resultSet = preparedStatement.executeQuery()) {
+            int tableSize = countTable(resultSet);
+            int index = 0;
+            String[] tableNames = new String[tableSize];
+            if (resultSet.isBeforeFirst()) {
+                while (resultSet.next()) {
+                    tableNames[index++] = resultSet.getString(1);
+                }
+                return tableNames;
+            } else return new String[0];
+        }
+    }
+
+    private int countTable(ResultSet resultSet) throws SQLException {
+        int count = 0;
+        while (resultSet.next()) {
+            count++;
+        }
+        resultSet.beforeFirst();
+        return count;
     }
 }
--- src/test/java/juja/sqlcmd/DatabaseManagerTest.java
+++ src/test/java/juja/sqlcmd/DatabaseManagerTest.java
@@ -1,38 +1,97 @@
 package juja.sqlcmd;
 
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class DatabaseManagerTest {
 
-    private DatabaseManager manager;
+    private static final String DB_NAME = "sqlcmd";
+    private static final String TEST_DB_NAME = "test";
+    private static final String DB_USER_NAME = "sqlcmd";
+    private static final String DB_USER_PASSWORD = "sqlcmd";
+    private static final String JDBC_URL = "jdbc:postgresql://127.0.0.1:5432/";
+    private static Connection connection;
+
+    private DatabaseManager databaseManager;
+
+    @BeforeClass
+    public static void setTestingEnvironment() throws SQLException {
+        connection = DriverManager.getConnection(
+                JDBC_URL, DB_USER_NAME, DB_USER_PASSWORD);
+        executeSqlQuery("DROP DATABASE IF EXISTS " + TEST_DB_NAME);
+        executeSqlQuery("CREATE DATABASE " + TEST_DB_NAME);
+        connection.close();
+        connection = DriverManager.getConnection(
+                JDBC_URL + TEST_DB_NAME, DB_USER_NAME, DB_USER_PASSWORD);
+    }
+
+    @AfterClass
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
 
     @Before
-    public void setUp() {
-        manager = new DatabaseManager();
+    public void setUp() throws SQLException {
+        databaseManager = new DatabaseManager();
+        databaseManager.connect(TEST_DB_NAME, DB_USER_NAME, DB_USER_PASSWORD);
+        dropAllTables();
     }
 
+    private static void executeSqlQuery(String sqlQuery) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sqlQuery);
+        }
+    }
 
-    @Test
-    public void connectWithValidParametersShouldReturnTrue() {
-        assertTrue(manager.connect("sqlcmd", "sqlcmd", "sqlcmd"));
+    private static void dropAllTables() throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String sqlQuery = "DROP TABLE IF EXISTS CASCADE";
+            statement.executeUpdate(sqlQuery);
+        }
     }
 
     @Test
-    public void connectWithWrongDatabaseNameShouldReturnFalse() {
-     assertFalse(manager.connect("wrongName", "sqlcmd", "sqlcmd"));
+    public void connect_WithValidParameters_ShouldReturnTrue() {
+        assertTrue(databaseManager.connect(DB_NAME, DB_USER_NAME, DB_USER_PASSWORD));
     }
 
     @Test
-    public void connectWithWrongUserNameShouldReturnFalse() {
-        assertFalse(manager.connect("sqlcmd", "wrongUserName", "sqlcmd"));
+    public void connect_WithWrongDatabaseName_ShouldReturnFalse() {
+        assertFalse(databaseManager.connect("wrongName", DB_USER_NAME, DB_USER_PASSWORD));
     }
 
     @Test
-    public void connectWithWrongPasswordShouldReturnFalse() {
-        assertFalse(manager.connect("sqlcmd", "sqlcmd", "wrongPassword"));
+    public void connect_WithWrongUserName_ShouldReturnFalse() {
+        assertFalse(databaseManager.connect(DB_NAME, "wrongUserName", DB_USER_PASSWORD));
+    }
+
+    @Test
+    public void connect_WithWrongPassword_ShouldReturnFalse() {
+        assertFalse(databaseManager.connect("sqlcmd", "sqlcmd", "wrongPassword"));
+    }
+
+    @Test
+    public void getTableNames_WhenThereAreNoTablesInDatabase_ShouldReturnEmptyArray() throws SQLException {
+        String[] expected = new String[0];
+        assertArrayEquals("We Expected:", expected, databaseManager.getTableNames());
+    }
+
+    @Test
+    public void getTableNames_WhenThereAreTwoTablesInDatabase_ShouldReturnTableNamesArray() throws SQLException {
+        executeSqlQuery("CREATE TABLE public.test1()");
+        executeSqlQuery("CREATE TABLE public.test2()");
+        String[] expected = new String[]{"test1", "test2"};
+        assertArrayEquals(expected, databaseManager.getTableNames());
     }
 }
