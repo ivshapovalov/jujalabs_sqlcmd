783202f7d30c8afa847f0ffa809246a40f8b9d39
DenysMelnychenko <aspirant89@gmail.com>
2017-12-18 00:12:40 +0200

    #26 used multiple catch blocks and changed names of test methods according to Danil recommendations

--- src/main/java/juja/sqlcmd/DatabaseManager.java
+++ src/main/java/juja/sqlcmd/DatabaseManager.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DatabaseManager {
     private static final String DB_CONNECTION_URL = "jdbc:postgresql://127.0.0.1:5432/";
@@ -15,12 +14,10 @@ public class DatabaseManager {
     public boolean connect(String databaseName, String user, String password) {
         try {
             Class.forName(POSTGRESQL_DRIVER);
+            connection = DriverManager.getConnection(DB_CONNECTION_URL + databaseName, user, password);
         } catch (ClassNotFoundException e) {
             System.out.println("Please add JDBC jar to your project");
             return false;
-        }
-        try {
-            connection = DriverManager.getConnection(DB_CONNECTION_URL + databaseName, user, password);
         } catch (SQLException e) {
             System.out.println(String.format("Can not connect to %s with user = %s and password %s", databaseName, user, password));
             return false;
