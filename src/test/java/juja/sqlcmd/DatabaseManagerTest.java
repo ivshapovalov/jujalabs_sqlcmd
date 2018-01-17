03ef9e8da9c77b3110f8c34173bf2802a10c4856
Ivan Shapovalov <ivshapovalov@gmail.com>
2018-01-17 15:24:49 +0300

    test travis 17

--- src/test/java/juja/sqlcmd/DatabaseManagerTest.java
+++ src/test/java/juja/sqlcmd/DatabaseManagerTest.java
@@ -74,7 +74,7 @@ public class DatabaseManagerTest {
 
     @Test
     public void connectWhenInvalidUserPasswordReturnsFalse() {
-        assertFalse(databaseManager.connect("sqlcmd", "sqlcmd", "wrong_password"));
+        assertTrue(databaseManager.connect("sqlcmd", "sqlcmd", "wrong_password"));
     }
 
     @Test
